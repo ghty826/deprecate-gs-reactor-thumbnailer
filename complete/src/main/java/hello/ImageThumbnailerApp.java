bc011a978da6dfb6a90f207974981ae48484ff99
Jon Brisbin <jon@jbrisbin.com>
2014-08-19 16:27:15 -0500

    Fixes #1 moves error handling to on the Stream, right before the consume(), which is where it should have been to begin with.

--- complete/src/main/java/hello/ImageThumbnailerApp.java
+++ complete/src/main/java/hello/ImageThumbnailerApp.java
@@ -36,69 +36,69 @@ import static reactor.event.selector.Selectors.$;
 @EnableReactor
 public class ImageThumbnailerApp {
 
-  @Bean
-  public Reactor reactor(Environment env) {
-    Reactor reactor = Reactors.reactor(env, Environment.THREAD_POOL);
+	@Bean
+	public Reactor reactor(Environment env) {
+		Reactor reactor = Reactors.reactor(env, Environment.THREAD_POOL);
 
-    // Register our thumbnailer on the Reactor
-    reactor.receive($("thumbnail"), new BufferedImageThumbnailer(250));
+		// Register our thumbnailer on the Reactor
+		reactor.receive($("thumbnail"), new BufferedImageThumbnailer(250));
 
-    return reactor;
-  }
+		return reactor;
+	}
 
-  @Bean
-  public ServerSocketOptions serverSocketOptions() {
-    return new NettyServerSocketOptions()
-        .pipelineConfigurer(pipeline -> pipeline.addLast(new HttpServerCodec())
-                                                .addLast(new HttpObjectAggregator(16 * 1024 * 1024)));
-  }
+	@Bean
+	public ServerSocketOptions serverSocketOptions() {
+		return new NettyServerSocketOptions()
+				.pipelineConfigurer(pipeline -> pipeline.addLast(new HttpServerCodec())
+				                                        .addLast(new HttpObjectAggregator(16 * 1024 * 1024)));
+	}
 
-  @Bean
-  public NetServer<FullHttpRequest, FullHttpResponse> restApi(Environment env,
-                                                              ServerSocketOptions opts,
-                                                              Reactor reactor,
-                                                              CountDownLatch closeLatch) throws InterruptedException {
-    AtomicReference<Path> thumbnail = new AtomicReference<>();
+	@Bean
+	public NetServer<FullHttpRequest, FullHttpResponse> restApi(Environment env,
+	                                                            ServerSocketOptions opts,
+	                                                            Reactor reactor,
+	                                                            CountDownLatch closeLatch) throws InterruptedException {
+		AtomicReference<Path> thumbnail = new AtomicReference<>();
 
-    NetServer<FullHttpRequest, FullHttpResponse> server = new TcpServerSpec<FullHttpRequest, FullHttpResponse>(NettyTcpServer.class)
-        .env(env).dispatcher("sync").options(opts)
-        .consume(ch -> {
-          // attach an error handler
-          ch.when(Throwable.class, ImageThumbnailerRestApi.errorHandler(ch));
+		NetServer<FullHttpRequest, FullHttpResponse> server = new TcpServerSpec<FullHttpRequest, FullHttpResponse>(
+				NettyTcpServer.class)
+				.env(env).dispatcher("sync").options(opts)
+				.consume(ch -> {
+					// filter requests by URI via the input Stream
+					Stream<FullHttpRequest> in = ch.in();
 
-          // filter requests by URI
-          Stream<FullHttpRequest> in = ch.in();
+					// serve image thumbnail to browser
+					in.filter((FullHttpRequest req) -> ImageThumbnailerRestApi.IMG_THUMBNAIL_URI.equals(req.getUri()))
+					  .when(Throwable.class, ImageThumbnailerRestApi.errorHandler(ch))
+					  .consume(ImageThumbnailerRestApi.serveThumbnailImage(ch, thumbnail));
 
-          // serve image thumbnail to browser
-          in.filter((FullHttpRequest req) -> ImageThumbnailerRestApi.IMG_THUMBNAIL_URI.equals(req.getUri()))
-            .consume(ImageThumbnailerRestApi.serveThumbnailImage(ch, thumbnail));
+					// take uploaded data and thumbnail it
+					in.filter((FullHttpRequest req) -> ImageThumbnailerRestApi.THUMBNAIL_REQ_URI.equals(req.getUri()))
+					  .when(Throwable.class, ImageThumbnailerRestApi.errorHandler(ch))
+					  .consume(ImageThumbnailerRestApi.thumbnailImage(ch, thumbnail, reactor));
 
-          // take uploaded data and thumbnail it
-          in.filter((FullHttpRequest req) -> ImageThumbnailerRestApi.THUMBNAIL_REQ_URI.equals(req.getUri()))
-            .consume(ImageThumbnailerRestApi.thumbnailImage(ch, thumbnail, reactor));
+					// shutdown this demo app
+					in.filter((FullHttpRequest req) -> "/shutdown".equals(req.getUri()))
+					  .consume(req -> closeLatch.countDown());
+				})
+				.get();
 
-          // shutdown this demo app
-          in.filter((FullHttpRequest req) -> "/shutdown".equals(req.getUri()))
-            .consume(req -> closeLatch.countDown());
-        })
-        .get();
+		server.start().await();
 
-    server.start().await();
+		return server;
+	}
 
-    return server;
-  }
+	@Bean
+	public CountDownLatch closeLatch() {
+		return new CountDownLatch(1);
+	}
 
-  @Bean
-  public CountDownLatch closeLatch() {
-    return new CountDownLatch(1);
-  }
+	public static void main(String... args) throws InterruptedException {
+		ApplicationContext ctx = SpringApplication.run(ImageThumbnailerApp.class, args);
 
-  public static void main(String... args) throws InterruptedException {
-    ApplicationContext ctx = SpringApplication.run(ImageThumbnailerApp.class, args);
-
-    // Reactor's TCP servers are non-blocking so we have to do something to keep from exiting the main thread
-    CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
-    closeLatch.await();
-  }
+		// Reactor's TCP servers are non-blocking so we have to do something to keep from exiting the main thread
+		CountDownLatch closeLatch = ctx.getBean(CountDownLatch.class);
+		closeLatch.await();
+	}
 
 }
